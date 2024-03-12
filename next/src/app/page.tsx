8bb831cb33ced26789e0929164ad6deec4448324
Hassan Waqar <hassanwaqar205@outlook.com>
2024-03-12 22:10:58 +0500

    Minor changes

---
+++ next/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function page() {
+  return (
+    <main>
+      <div className="border-2 border-red-400 p-1 m-16 mx-64 bg-zinc-900">
+        <h1 className="text-center items-center justify-center align-middle text-6xl text-cyan-400 ">
+          Hassan Waqar
+        </h1>
+      </div>
+    </main>
+  );
+}
