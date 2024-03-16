ca219676076e025d2a6970cb5e88017015083e19
Hassan Waqar <hassanwaqar205@outlook.com>
2024-03-16 15:06:47 +0500

    nextjs app setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function page() {
+  return (
+    <div  >
+      <h1 className='text-2xl text-center bg-cyan-300 text-white'>Hassan Waqar</h1>
+    </div>
+  )
+}
