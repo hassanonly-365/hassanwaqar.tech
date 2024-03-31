72aaec6b8757e619bf8127cf9ae66e523f9bc387
Hassan Waqar <hassanwaqar205@outlook.com>
2024-03-31 15:14:33 +0500

    initial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div  >
-      <h1 className='text-2xl text-center bg-cyan-300 text-white'>Hassan Waqar</h1>
+    <div>
+      <h1>Testing</h1>
     </div>
   )
 }
