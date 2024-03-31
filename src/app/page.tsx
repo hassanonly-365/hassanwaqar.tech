4627d5a6ece5f5e25bf1220f4cde05ab02e0277b
Hassan Waqar <hassanwaqar205@outlook.com>
2024-03-31 19:53:33 +0500

    updated page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>
+    <div className='items-center justify-center align-middle'>
       <h1>Testing</h1>
     </div>
   )
