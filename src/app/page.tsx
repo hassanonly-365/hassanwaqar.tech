c18020dc332952203e0458b00ef00a0cb747961c
Hassan Waqar <hassanwaqar205@outlook.com>
2024-04-01 11:42:10 +0500

    update page.tsx & layout.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import React from "react";
 export default function page() {
   return (
     <main>
-      <div className="items-center justify-center align-middle">
+      <div className="flex items-center justify-center m-16">
+      <div className="border border-white p-2 text-3xl">
         <h1>Hassan Waqar</h1>
+      </div>
       </div>
     </main>
   );
